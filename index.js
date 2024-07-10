f0ba596bbd17ce452ab9855b429a7dc3f7754ef8
yimengqiandong <chc1718345936626@163.com>
2024-07-10 10:22:19 +0800

    增加小游戏网页

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function ty() {
     setTimeout("cyx()", 160);
   } else if (text == "CWX") {
     setTimeout('alert("我的微信是：yimengqiandong(添加微信请注明身份)")', 100);
+  } else if (text == "GAME") {
+    setTimeout(
+      'alert("即将跳转到小游戏,（尚未完善BUG非常多，自己去探索吧）！！！")',
+      100
+    );
+    setTimeout('window.open("GAME.html");', 200);
   } else if (text != "CZQ") {
     setTimeout('alert("该密钥输入错误或不存在！")', 160);
     document.getElementById("myInput").value = "";
@@ -20,4 +26,4 @@ function cyx() {
   alert("我的邮箱是：chc1718345936626@163.com");
 }
 
-//CZQ,CYX，CWX
+//CZQ,CYX，CWX，GAME
